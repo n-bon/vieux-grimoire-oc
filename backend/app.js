92564d7667d8e4fd7247be750f80409ec6634743
Nathan Bonin <n.bonin@gmx.fr>
2025-06-09 16:21:16 +0200

    Build (app.js) Add status to respnse

--- backend/app.js
+++ backend/app.js
@@ -3,8 +3,13 @@ const express = require('express');
 const app = express();
 
 
-app.use((req, res) => {
-    res.json({ message : 'Requête bien reçue dans le fichier app.js'});
+app.use((req, res, next) => {
+    res.json({ message : 'Requête bien reçue dans le fichier app.js 2'});
+    next();
+});
+
+app.use((req, res, next) => {
+    res.status(201);
 });
 
 module.exports = app;
