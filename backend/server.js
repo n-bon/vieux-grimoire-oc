3cc2ea99eb93e8c90f3d4d0cebbe745886eecec5
Nathan Bonin <n.bonin@gmx.fr>
2025-06-09 16:02:12 +0200

    Build (server.js) Add first respond using node server

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,7 @@
-//server file called in npm init
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+    res.end('Voilà la réponse du server');
+});
+
+server.listen(process.env.PORT || 3000);
