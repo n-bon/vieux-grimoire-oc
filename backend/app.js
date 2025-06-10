290b3f67e423d014abef783d8449960101d8a10f
Nathan Bonin <n.bonin@gmx.fr>
2025-06-10 18:12:26 +0200

    Build (app.js) Add fake books in route get

--- backend/app.js
+++ backend/app.js
@@ -3,13 +3,40 @@ const express = require('express');
 const app = express();
 
 
-app.use((req, res, next) => {
-    res.json({ message : 'Requête bien reçue dans le fichier app.js 4'});
-    next();
-});
-
-app.use((req, res, next) => {
-    res.status(201);
+app.get('/api/books/', (req, res, next) => {
+    const books = [
+        {
+            userId: '1234',
+            title: 'Le petit prince',
+            author: 'de Saint-Exupéry, Antoine',
+            imageUrl: 'https://static.livre-rare-book.com/pictures/TBW/16996_2.jpg',
+            year: 1943,
+            genre: 'Pour enfants',
+            ratings : [
+                {
+                    userId: '3456',
+                    grade: 3,
+                }
+            ],
+            averageRating: 3
+        },
+        {
+            userId: '3456',
+            title: 'Pierre et Jean',
+            author: 'de Maupassant, Guy',
+            imageUrl: 'https://www.librairie-faustroll.com/21234-home_default/maupassant-guy-de-pierre-et-jean-ollendorff-1888-edition-originale-relie-en-demi-percaline.jpg',
+            year: 1888,
+            genre: 'Fiction',
+            ratings : [
+                {
+                    userId: '1234',
+                    grade: 5,
+                }
+            ],
+            averageRating: 5
+        }
+    ]
+    res.status(200).json(books);
 });
 
 module.exports = app;
