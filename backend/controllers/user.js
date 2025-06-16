16b9eba1e105f59319013986ee8d6e7edcfb7b52
Nathan Bonin <n.bonin@gmx.fr>
2025-06-16 16:17:33 +0200

    Feat (ctrl/user.js) Add signup logic

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -1,6 +1,18 @@
+const bcrypt = require('bcrypt');
+const User = require('../models/user');
 
 exports.signup = (req, res, next) => {
-
+    bcrypt.hash(req.body.password, 10)
+        .then(hash => {
+            const user = new User({
+                email: req.body.email,
+                password: hash
+            });
+            user.save()
+                .then(() => res.status(201).json({ message: 'User created' }))
+                .catch(error => res.status(400).json({ error }));
+        })
+        .catch(error => res.status(500).json({ error }));
 };
 
 exports.login = (req, res, next) => {
