55aaed28e925dccb786923e9f7eb82e220e431ca
Nathan Bonin <n.bonin@gmx.fr>
2025-06-16 23:50:42 +0200

    Feat (backend) Add functionnal create book route using multer & sharp

--- backend/controllers/book.js
+++ backend/controllers/book.js
@@ -1,9 +1,63 @@
+const fs = require('fs');
+const path = require('path');
 const sharp = require('sharp');
+
 const Book = require('../models/book');
 
-exports.createBook = (req, res, next) => {
+exports.createBook = (req, res, next) => {    
+    if (!req.file) {
+        return res.status(400).json({ message: 'No image found' });
+    }
 
-}
+    let bookObject;
+    try {
+        bookObject = JSON.parse(req.body.book);
+    } catch (parseError) {
+        return res.status(400).json({ message : 'Book data not valid' });
+    }
+    //if book data is valid : 
+    //1-- converting & saving image
+    const originalFilename = req.file.filename;
+    const inputPath = path.join(__dirname, '..', 'images', originalFilename);
+    const filenameWithoutExt = path.parse(originalFilename).name;
+    const outputFilename = filenameWithoutExt + '.webp';
+    const outputPath = path.join(__dirname, '..', 'images', outputFilename);
+
+    sharp(inputPath)
+        .resize(976, 1190)
+        .toFormat('webp')
+        .toFile(outputPath)
+        .then(() => {
+            fs.unlink(inputPath, (unlinkErr) => {
+                if (unlinkErr) {
+                    console.error('Error while trying to delete the original file : ', unlinkErr);
+                }
+            });
+            //2-- formatting data
+            const imageURL = `${req.protocol}://${req.get('host')}/images/${outputFilename}`;
+            const initialRating = parseFloat(bookObject.ratings?.[0]?.grade)
+            const year = parseInt(bookObject.year);
+            const userId = req.auth.userId;
+
+            const book = new Book ({
+                userId: userId,
+                title: bookObject.title,
+                author: bookObject.author,
+                imageUrl: imageURL,
+                year: year,
+                genre: bookObject.genre,
+                ratings: [
+                    { userId: userId, grade: initialRating }
+                ],
+                averageRating: initialRating
+            });
+            return book.save();
+        })
+        .then(() => res.status(201).json({ message : "Book created"}))
+        .catch((error) => {
+            res.status(400).json({ error });
+        });
+};
 
 exports.readAllBooks = (req, res, next) => {
     const books = [
--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -2,7 +2,7 @@ const jwt = require('jsonwebtoken');
 
 module.exports = (req, res, next) => {
     try {
-        const token = req.header.authorization.split(' ')[1];
+        const token = req.headers.authorization.split(' ')[1];
         const decodedToken = jwt.verify(token, 'TEMPORARY_SECRET_KEY');
         const userId = decodedToken.userId;
         req.auth = {
