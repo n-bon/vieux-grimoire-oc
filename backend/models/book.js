63597cb486683972e78105fe9ff415f51b805844
Nathan Bonin <n.bonin@gmx.fr>
2025-06-16 15:29:44 +0200

    Feat (model/book.js) Add average rating

--- backend/models/book.js
+++ backend/models/book.js
@@ -7,7 +7,8 @@ const bookSchema = mongoose.Schema({
     imageUrl: {type: String, required: true},
     year: {type: Number, required: true},
     genre: {type: String, required: true},
-    rating: {type: Array, required: true}
+    rating: {type: Array, required: true},
+    averageRating: {type: Number, required: true}
 });
 
 module.exports = mongoose.model('Book', bookSchema);
