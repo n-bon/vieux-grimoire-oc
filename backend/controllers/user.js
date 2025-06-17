63488ad17407996ac312e5e1b08b75ff3c5d2fe1
Nathan Bonin <n.bonin@gmx.fr>
2025-06-17 02:10:03 +0200

    Fix (backend) Changing userId declaration when login to fit with frontend behaviour

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -31,7 +31,7 @@ exports.login = (req, res, next) => {
                     res.status(200).json({
                         userId: user._id,
                         token: jwt.sign(
-                            { userId: user._id },
+                            { user: { userId: user._id.toString() } },
                             'TEMPORARY_SECRET_KEY',
                             { expiresIn : '72h' }
                         )
--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -4,7 +4,7 @@ module.exports = (req, res, next) => {
     try {
         const token = req.headers.authorization.split(' ')[1];
         const decodedToken = jwt.verify(token, 'TEMPORARY_SECRET_KEY');
-        const userId = decodedToken.userId;
+        const userId = decodedToken.user.userId;
         req.auth = {
             userId: userId
         };
