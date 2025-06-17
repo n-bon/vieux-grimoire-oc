f24b0e4f4c6c6a45e5007fe4f18ed9b9fa91888f
Nathan Bonin <n.bonin@gmx.fr>
2025-06-17 18:08:55 +0200

    Fix (multer-config) Change image filename construction

--- backend/middleware/multer-config.js
+++ backend/middleware/multer-config.js
@@ -1,4 +1,5 @@
 const multer = require('multer');
+const path = require('path');
 
 const MIME_TYPES = {
     'image/jpg': 'jpg',
@@ -12,9 +13,9 @@ const storage = multer.diskStorage({
         callback(null, 'images');
     },
     filename: (req, file, callback) => {
-        const name = file.originalname.split(' ').join('_');
+        const nameWithoutExt = path.parse(file.originalname).name.split(' ').join('_');
         const extension = MIME_TYPES[file.mimetype];
-        callback(null, name + Date.now() + '.' + extension);
+        callback(null, nameWithoutExt + Date.now() + '.' + extension);
     }
 });
 
