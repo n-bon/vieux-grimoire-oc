5b20c207807a77d03b821e25aa9fff7073532db8
Nathan Bonin <n.bonin@gmx.fr>
2025-06-17 02:14:43 +0200

    Fix (app.js) Change syntax error in CROSS origin middleware

--- backend/app.js
+++ backend/app.js
@@ -24,7 +24,7 @@ app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
     //4 methods mentionned in docs :
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST', 'PUT', 'DELETE');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
     next();
 });
 
